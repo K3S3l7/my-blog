99e9eaa39c094b0804af41a2b913ddcbe567e51e
Karim <kimo@Kimu.local>
2026-05-30 12:12:50 +0100

    Add CNAME file and remove basePath for custom domain

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@ const nextConfig = {
   output: "export",
   trailingSlash: true,
   images: { unoptimized: true },
-  // Repo is K3S3l7/my-blog so the site lives at /my-blog
-  basePath: "/my-blog",
-  assetPrefix: "/my-blog/",
 };
 
 module.exports = nextConfig;
